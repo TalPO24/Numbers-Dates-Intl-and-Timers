7e0fc6054c77551693c0c7340353220573341999
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-21 14:20:27 +0300

    Numbers Dates Intl and Timers

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ const account1 = {
         '2019-12-23T07:42:02.383Z',
         '2020-01-28T09:15:04.904Z',
         '2020-04-01T10:17:24.185Z',
-        '2020-05-08T14:11:59.604Z',
-        '2020-05-27T17:01:17.194Z',
-        '2020-07-11T23:36:17.929Z',
-        '2020-07-12T10:51:36.790Z',
+        '2023-04-14T14:11:59.604Z',
+        '2023-04-16T17:01:17.194Z',
+        '2023-04-19T23:36:17.929Z',
+        '2023-04-21T10:51:36.790Z',
     ],
     currency: 'EUR',
     locale: 'pt-PT', // de-DE
@@ -34,14 +34,14 @@ const account2 = {
     pin: 2222,
 
     movementsDates: [
-        '2019-11-01T13:15:33.035Z',
-        '2019-11-30T09:48:16.867Z',
-        '2019-12-25T06:04:23.907Z',
-        '2020-01-25T14:18:46.235Z',
-        '2020-02-05T16:33:06.386Z',
-        '2020-04-10T14:43:26.374Z',
-        '2020-06-25T18:49:59.371Z',
-        '2020-07-26T12:01:20.894Z',
+        "2019-11-01T13:15:33.035Z",
+        "2019-11-30T09:48:16.867Z",
+        "2019-12-25T06:04:23.907Z",
+        "2020-01-25T14:18:46.235Z",
+        "2020-02-05T16:33:06.386Z",
+        "2020-04-10T14:43:26.374Z",
+        "2020-06-25T18:49:59.371Z",
+        "2020-07-26T12:01:20.894Z",
     ],
     currency: 'USD',
     locale: 'en-US',
@@ -78,6 +78,25 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 // Functions
 
+const formatMovementDate = function(date, locale) {
+    const calcDaysPassed = (date1, date2) =>
+        Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+    const daysPassed = calcDaysPassed(new Date(), date);
+    console.log(daysPassed);
+
+    if (daysPassed === 0) return "Today";
+    if (daysPassed === 1) return "Yesterday";
+    if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+    // return new Intl.DateTimeFormat(locale).format(date);
+};
+
+
 const displayMovements = function(acc, sort = false) {
     containerMovements.innerHTML = '';
 
@@ -87,12 +106,7 @@ const displayMovements = function(acc, sort = false) {
         const type = mov > 0 ? 'deposit' : 'withdrawal';
 
         const date = new Date(acc.movementsDates[i])
-        const day = `${date.getDate()}`.padStart(2, 0)
-        const month = `${date.getMonth() + 1}`.padStart(2, 0)
-        const year = date.getFullYear()
-
-        const displayDate = `${day}/${month}/${year}`
-
+        const displayDate = formatMovementDate(date)
 
         const html = `
       <div class="movements__row">
@@ -471,3 +485,14 @@ console.log(Date.now()) // Time stamp
 future.setFullYear(2040)
 console.log(future)
 */
+
+//* Operations with Dates
+
+const future = new Date(2037, 10, 19, 15, 23)
+console.log(+future)
+
+const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24)
+
+const days1 = calcDaysPassed(new Date(2037, 3, 4),
+    new Date(2037, 3, 14))
+console.log(days1)
