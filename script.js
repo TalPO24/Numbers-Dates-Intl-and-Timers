60c3841dbf406b1ee7ea2ab013f0861630b56d92
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-23 12:05:24 +0300

    Numbers Dates Intl and Timers

--- script.js
+++ script.js
@@ -49,7 +49,6 @@ const account2 = {
 
 const accounts = [account1, account2];
 
-/////////////////////////////////////////////////
 // Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
@@ -77,7 +76,6 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 // Functions
-
 const formatMovementDate = function(date, locale) {
     const calcDaysPassed = (date1, date2) =>
         Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
@@ -89,11 +87,12 @@ const formatMovementDate = function(date, locale) {
     if (daysPassed === 1) return "Yesterday";
     if (daysPassed <= 7) return `${daysPassed} days ago`;
 
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    // const day = `${date.getDate()}`.padStart(2, 0);
+    // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    // const year = date.getFullYear();
+    // return `${day}/${month}/${year}`;
     // return new Intl.DateTimeFormat(locale).format(date);
+    return new Intl.DateTimeFormat(locale).format(date)
 };
 
 
@@ -106,7 +105,7 @@ const displayMovements = function(acc, sort = false) {
         const type = mov > 0 ? 'deposit' : 'withdrawal';
 
         const date = new Date(acc.movementsDates[i])
-        const displayDate = formatMovementDate(date)
+        const displayDate = formatMovementDate(date, acc.locale)
 
         const html = `
       <div class="movements__row">
@@ -197,14 +196,28 @@ btnLogin.addEventListener('click', function(e) {
       currentAccount.owner.split(' ')[0]
     }`;
         containerApp.style.opacity = 100;
-        // Create current date and time
+        //* Create current date and time
+        // const now = new Date()
+        // const day = `${now.getDate()}`.padStart(2, 0)
+        // const month = `${now.getMonth() + 1}`.padStart(2, 0)
+        // const year = now.getFullYear()
+        // const hours = `${now.getHours()}`.padStart(2, 0)
+        // const min = `${now.getMinutes()}`.padStart(2, 0)
+        // labelDate.textContent = `${day}/${month}/${year}, ${hours}:${min}`
         const now = new Date()
-        const day = `${now.getDate()}`.padStart(2, 0)
-        const month = `${now.getMonth() + 1}`.padStart(2, 0)
-        const year = now.getFullYear()
-        const hours = `${now.getHours()}`.padStart(2, 0)
-        const min = `${now.getMinutes()}`.padStart(2, 0)
-        labelDate.textContent = `${day}/${month}/${year}, ${hours}:${min}`
+        const options = {
+                hour: 'numeric',
+                minute: 'numeric',
+                day: 'numeric',
+                month: 'numeric',
+                year: 'numeric',
+                // weekday: 'long'
+            }
+            // const local = navigator.language
+
+        labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, options).format(now)
+
+
 
         // Clear input fields
         inputLoginUsername.value = inputLoginPin.value = '';
